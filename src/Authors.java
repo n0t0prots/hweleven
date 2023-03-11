e4354ed7a1e7b63963e585b2417bb0c99a0faef3
n0t0prots <artemprocenko498@gmail.com>
2023-03-11 22:37:41 +0200

    Initial commit

---
+++ src/Authors.java
@@ -0,0 +1,61 @@
+
+import java.awt.print.Book;
+import java.util.List;
+
+public class Authors {
+    private Long id;
+    private String name;
+    private String lastName;
+    private List<Book> books;
+
+    public Authors(Long id, String name, String lastName) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Authors( String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
---
+++ src/Books.java
@@ -0,0 +1,48 @@
+
+public class Books {
+    private Long id;
+    private String title;
+    private Authors author;
+
+    public Books(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public Books( String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Authors getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Authors author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author=" + author +
+                '}';
+    }
+}
